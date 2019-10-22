31ab3a7c5682e10c26d289ec522ebfb5247e2a41
Moritz Hartmann <moritz@thinksono.com>
2019-10-22 22:07:41 +0100

    Add legal text for Privacy Policy

--- faithbook-web/src/Legals.js
+++ faithbook-web/src/Legals.js
@@ -11,7 +11,141 @@ export const Terms =() => {
 export const Privacy =() => {
   return (
     <div className="Legals">
-      Privacy
+      <h1 className="Title">Privacy Policy</h1>
+        <div>
+          <p><strong>Last Modified:</strong>&nbsp;October 22, 2019</p>
+          <p>BibleFeed Ltd. is a media company. &nbsp;Our mission is to inspire the world by values.</p>
+          <p>The services that link to this Privacy Policy provide values and inspirations for all kind of aspects of human life.</p>
+          <p>When you use these services, you’ll share some information with us. We want to explain clearly what information we collect, why we collect it, how we use it, whom we share it with, and the controls we give you to access, update, and delete your information.</p>
+          <p>That’s why we’ve written this Privacy Policy. We tried to write it in simple language with minimal legalese so it’s easy for you to understand. If you have any questions about anything in our Privacy Policy, please <a href="#!">contact us</a>.&nbsp;</p>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>Information We Collect</h2>
+          <p>There are two basic categories of information we collect:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p>Information we get when you use our services</p>
+              </li>
+              <li>
+                  <p>Information we get from third parties</p>
+              </li>
+          </ul>
+          <p>Here’s how we collect your information.</p>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>How We Collect Information</h2>
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Information We Get When You Use Our Services</h3>
+          <p>When you use our services, we collect information about which services you use and how you use them. For example, we may know what section or post you are reading, when you’re reading it, and how you’re reading it (desktop, mobile browser, or mobile app). Here’s a more detailed explanation of the information we collect when you use our services:</p>
+          <p><strong>Usage Information.</strong> How you interact with our services, such as what sections you look at.</p>
+          <p><strong>Location Information.</strong> &nbsp;When you use our services, we may collect information about your location.</p>
+          <p><strong>Information Collected by Cookies and Other Technologies.</strong> We may use cookies and other technologies such as web beacons, web storage, and unique advertising identifiers to collect information about your activity, browser, and device. We may use these technologies to collect information when you interact with services we offer through one of our partners, such as advertising and commerce features. Most web browsers are set to accept cookies by default. It is up to you to move or reject browser cookies through the settings on your browser or device. Removing or rejecting cookies may affect our service function and availability.</p>
+          <p><strong>Log Information.</strong> &nbsp;We also collect log information when you use our services. For example:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p>Details about how you’ve used our services.</p>
+              </li>
+              <li>
+                  <p>Device information, such as web browser type and language.</p>
+              </li>
+              <li>
+                  <p>Access times.</p>
+              </li>
+              <li>
+                  <p>Pages and content viewed.</p>
+              </li>
+              <li>
+                  <p>IP address.</p>
+              </li>
+              <li>
+                  <p>Identifiers associated with cookies or other technologies that may uniquely identify your device or browser.</p>
+              </li>
+              <li>
+                  <p>Pages you visited before or after visiting our website or using our service.</p>
+              </li>
+          </ul>
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Information We Collect From Third Parties</h3>
+          <p>We may collect information from our affiliates and third parties. For example:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p>If you interact with one of our advertisers, they may share information with us to help target or measure ad performance.</p>
+              </li>
+          </ul>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>How We Use Information</h2>
+          <p>We use your information to provide you with the best products and services we can build and improve. Here’s what we use this information for:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p>To develop, maintain, improve, deliver, and protect our products and services.</p>
+              </li>
+              <li>
+                  <p>Monitor and analyze trends and usages of our products and services.</p>
+              </li>
+              <li>
+                  <p>Personalize our services by showing you better and more relevant content.</p>
+              </li>
+              <li>
+                  <p>Provide ad targeting and measurement to show relevant ads.</p>
+              </li>
+              <li>
+                  <p>Enhance and enforce the safety and security of our products and services.</p>
+              </li>
+          </ul>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>How We Share Information</h2>
+          <p>We may share information about you in the following ways:</p>
+          <p><strong>With service providers.</strong> We may share information about you with service providers who perform services on our behalf.&nbsp;</p>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>Third-Party Content and Integrations</h2>
+          <p>Our services may contain third-party links and search results, include third-party integrations, or offer a co-branded or third-party-branded-service. Through these links, third-party integrations, and co-branded or third-party-branded services, you may be providing information, including personal information, directly to the third party, to us, or to both. You acknowledge and agree that we are not responsible for how those third parties collect or use your information. We encourage you to review the privacy policies of every third-party service you visit or use, including those third parties you interact with through our services.</p>
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>Analytics and Advertising Services</h2>
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Provided by Others</h3>
+          <p>We may let other companies use cookies, web beacons, and similar technologies on our services. These companies may collect information about how you use our services over time, and combine it with similar information from other services and companies. This information may be used to analyze and track data, determine the popularity of certain content, and under your online activity, among other things.</p>
+          <p>Some companies may use information collected on our services to measure the performance of ads and deliver more relevant ads, including on third-party websites and apps.</p>
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Provided by Us</h3>
+          <p>We may collect information about your activity on third-party services that use cookies and other technologies provided by us. We use this information to improve your user experience with our services and to improve your advertising services, including measuring the performance of ads and showing you relevant ads. Visit our Manage Consent page to learn more about advertising and how you can control the information used to select the ads you see.</p>
+
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>How Long We Keep Your Information</h2>
+          <p>We store your information, the posts you upload, and the comments you upload for a period of time. For example:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p>We store logging information for different lengths of time based on how precise it is.</p>
+              </li>
+          </ul>
+          <p>There may be legal requirements to store your data and we may need to suspend those deletion practices if we receive reports of abuse or other Terms of Service violations. We may also need to retain certain information in backup for a limited period of time, or as required by law.</p>
+
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>Control over Your Information</h2>
+          <p>Your privacy is very important to us. We want you to have full control over the information you give us, the information we use, and the information we keep. These are the tools we provide you to control your information:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p><strong>Removing Permissions.</strong> If you agree to let us use your information, you can change your mind at any time by changing the settings on our Manage Consent page. If you remove permission, certain services may be impacted and may lose functionality.</p>
+              </li>
+          </ul>
+
+          <h2 className="Headline" style={{"white-space": "pre-wrap"}}>Users in the European Union and European Economic Area</h2>
+          <p>If you’re a user in the European Union or European Economic Area, please note that BibleFeed Ltd. is the data controller of your information. Here’s additional information we would like you to know:</p>
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Bases For Using Your Information</h3>
+          <p>Your country only allows us to use your personal information when certain conditions apply or are met. These conditions are called legal bases. At 9GAG, we rely on one of the following bases:</p>
+          <ul data-rte-list="default">
+              <li>
+                  <p><strong>Legitimate Interest.</strong> We may use your information because we - or a third party - has a legitimate interest. For example, we need to use your information to provide, maintain and improve our services. To keep the lights on at BibleFeed HQ, we may use this information to show you relevant ads.</p>
+              </li>
+              <li>
+                  <p><strong>Consent. </strong>We may ask for your consent to use your information for specific purposes. If we do, we’ll make sure you can revoke your consent in our services or through your device permissions at any time.</p>
+              </li>
+              <li>
+                  <p><strong>Legal obligation.</strong> We may use your personal information when we are required to comply with the law, such as when we respond to valid legal process or need to take action to protect our users or employees.</p>
+              </li>
+          </ul>
+
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Your Right To Object</h3>
+          <p>You have the right to object to our use of your information. You can stop our use of your information by disabling the feature altogether. You can do this on our Manage Consent page.</p>
+
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>International Transfers</h3>
+          <p>We may collect your personal information from, transfer it to, and process it in the United States, United Kingdom and other countries outside of where you live. When we share information of EU and EEA users outside the EU or EEA, we make sure an adequate transfer mechanism is in place.</p>
+
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Your Right To Complain</h3>
+          <p>If you’re based in the EU or EEA and have a complaint, you can file your complaint with the supervisory authority in your Member States. For example, if you’re based in the UK, you can file a complaint with the Information Commissioner’s Office.</p>
+
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Children</h3>
+          <p>Our services are not intended for anyone under 13. We don’t direct our services to anyone under 13.</p>
+          <p>We do not knowingly collect personal information from anyone under 13. We may limit how we collect, use and store some of the information of EU or EEA users between ages 13 and 16. If we need to rely on consent as a legal bases for processing your information and your country requires consent from a parent, we may require your parent’s consent before we collect and use that information.</p>
+
+          <h3 className="Headline" style={{"white-space": "pre-wrap"}}>Revisions and Changes to the Privacy Policy</h3>
+          <p>We may change this Privacy Policy from time to time. When we do, we will let you know by notifying you one way or another. We may let you know by revising the date at the top of this privacy policy, which is available on our websites and mobile applications. We may also provide additional notice, such as adding a statement to our homepage, or providing you with an in-app notification.</p>
+      </div>
     </div>
   );
 }
