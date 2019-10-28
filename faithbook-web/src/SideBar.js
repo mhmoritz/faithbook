49c5ff9c2b9bbb2a78e4c957311eb192946b25ba
Moritz Hartmann <moritz@thinksono.com>
2019-10-28 16:36:11 +0000

    Fix linking from SideBar to /feed

--- faithbook-web/src/SideBar.js
+++ faithbook-web/src/SideBar.js
@@ -40,7 +40,7 @@ class SideBar extends Component {
     Object.keys(this.props.titles).forEach((key, cnt) => {
       const active = category === key;
       items.push(
-        <Link className={classes.link} key={cnt}>
+        <Link to="/feed" className={classes.link} key={cnt}>
           <ListItem button onClick={() => this.props.setFeed(key, this.props.translation)}>
             <div
               className={classes.circle}
